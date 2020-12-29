f6678ab42e01de958117d978c47d82c202c095d4
Khalak97 <valodyakh@gmail.com>
2020-12-29 14:58:16 +0200

    fix

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,6 +9,7 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__top">
         <img
+          className="sidebar__backImg"
           src="https://content.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/01-dsk-e6-v2.png.original.png"
           alt="background"
         />
@@ -18,15 +19,13 @@ function Sidebar() {
       </div>
 
       <div className="sidebar__stats">
-        <div className="sidebar__stats">
-          <div className="sidebar__stat">
-            <p>Who viewed you</p>
-            <p className="sidebar__statNumber">1234</p>
-          </div>
-          <div className="sidebar__stat">
-            <p>Views on post</p>
-            <p className="sidebar__statNumber">1234</p>
-          </div>
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">1234</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">1234</p>
         </div>
       </div>
 
